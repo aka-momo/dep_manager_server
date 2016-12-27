dfd2caa29949f3fc2008573a07de01e9bb31e9d9
Mohamed Diaa <mohamed.diaa27@gmail.com>
2016-12-27 09:24:41 +0200

    adding basic tests for api packages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      resources :packages do
+      resources :packages, only: [] do
         post :fetch, on: :collection
       end
     end
--- test/controllers/api/v1/packages_controller_test.rb
+++ test/controllers/api/v1/packages_controller_test.rb
@@ -1,9 +1,32 @@
 require 'test_helper'
 
 class Api::V1::PackagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should post fetch' do
-    # TODO: TESTS
-    post api_v1_packages_fetch_url
+  test 'should succesfully fetch' do
+    post fetch_api_v1_packages_path, packages:  ['nokogiri'],
+                                     language: 'ruby',
+                                     os: 'mac'
     assert_response :success
+  end
+
+  test 'should fail to fetch due to language' do
+    post fetch_api_v1_packages_path, packages:  ['nokogiri'],
+                                     language: 'python',
+                                     os: 'mac'
+    assert_response :bad_request
+  end
+
+  test 'should fail to fetch due to OS' do
+    post fetch_api_v1_packages_path, packages:  ['nokogiri'],
+                                     language: 'ruby',
+                                     os: 'windows'
+    assert_response :bad_request
+  end
+
+  test 'should fail to fetch due to packages' do
+    post fetch_api_v1_packages_path, packages:  1,
+                                     language: 'ruby',
+                                     os: 'mac'
+
+    assert_response :unprocessable_entity
   end
 end
